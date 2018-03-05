d8abcd6cdf513ea2f68794a2d891e28967f99dd5
jielin <781458938@qq.com>
2018-03-06 00:43:28 +0800

    add tools

--- src/main/java/jielin/snapshot/service/impl/ScriptServiceImpl.java
+++ src/main/java/jielin/snapshot/service/impl/ScriptServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class ScriptServiceImpl implements jielin.snapshot.service.ScriptService{
     @Override
     public Result runScript(String scriptName) {
-        String command = "powershell -File e:/scriptsUtils/" + scriptName+".ps1";
+        String command = "powershell.exe  E:/ScriptsUtils/" + scriptName+".ps1";
         Process process = null;
         try {
             process = Runtime.getRuntime().exec(command);
